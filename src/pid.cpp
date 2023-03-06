2d91888c0c275559eea0cc941dcd345e2d4b5523
Pandu Surya Tantra <pandustantra@gmail.com>
2023-03-06 17:49:14 +0700

    Menambah comment di kode program

--- src/pid.cpp
+++ src/pid.cpp
@@ -24,34 +24,44 @@ void pid::init(double kp, double ki, double kd, double dt, double min_output, do
 
 double pid::update(double setpoint, double input)
 {
+    /* Calculating the time difference between the last time
+    the function was called and the current time. */
     static ros::Time time_last, time_now;
-
     time_last = time_now;
     time_now = ros::Time::now();
+    ros::Duration dt = time_now - time_last;
 
+    /* Reset the error last and integral
+    if the time difference between the last time the function was
+    called and the current time is greater than 1 second. */
     if (time_now - time_last > ros::Duration(1.0))
     {
         _error_last = 0;
         _integral = 0;
     }
 
+    /* Calculating the PID's each term. */
     _error = setpoint - input;
     _propotional = _kp * _error;
     _derivative = _kd * (_error - _error_last) / _dt;
     _integral += _ki * _error * _dt;
 
+    /* This is a saturation function. It is used to limit the integral term. */
     if (_integral > _max_integral)
         _integral = _max_integral;
     else if (_integral < _min_integral)
         _integral = _min_integral;
 
+    /* This is the PID equation. */
     _output = _propotional + _derivative + _integral;
 
+    /* This is a saturation function. It is used to limit the PID output. */
     if (_output > _max_output)
         _output = _max_output;
     else if (_output < _min_output)
         _output = _min_output;
 
+    /* Saving the error from the last time the function was called. */
     _error_last = _error;
 
     return _output;
